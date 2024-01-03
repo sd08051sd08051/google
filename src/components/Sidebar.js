a788c842026e0b7d5f2c02c66ad035d4ac162398
sd08051sd08051 <southafrica.2018.durban@gmail.com>
2024-01-03 15:39:33 +0900

    SideBarIcon動かず

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { SidebarData } from "./SidebarData";
+// import SidebarIcon from "./SidebarIcon";
 
 function Sidebar() {
-  return <div className="Sidebar"></div>;
+  return (
+    <div className="Sidebar">
+      {/* <SidebarIcon /> */}
+      <ul className="SidebarList">
+        {SidebarData.map((value, key) => {
+          return (
+            <li>
+              <div id="icon">{value.icon}</div>
+              <div id="title">{value.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,7 +1,28 @@
 import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import AddCardIcon from "@mui/icons-material/AddCard";
+import FeedbackIcon from "@mui/icons-material/Feedback";
 
-function SidebarData() {
-  return <div>SidebarData</div>;
-}
-
-export default SidebarData;
+export const SidebarData = [
+  {
+    title: "Home",
+    icon: <HomeIcon />,
+    link: "/home",
+  },
+  {
+    title: "Support",
+    icon: <SupportAgentIcon />,
+    link: "/support",
+  },
+  {
+    title: "PaymentSetting",
+    icon: <AddCardIcon />,
+    link: "/card",
+  },
+  {
+    title: "Feedback",
+    icon: <FeedbackIcon />,
+    link: "/Feedback",
+  },
+];
